20864724bd2caa0d9e9d4adebcaa0ec273814f2c
kyle <kyledmclean@gmail.com>
2020-05-30 12:00:16 -0500

    Nonsense.

--- lib/apiconnect.rb
+++ lib/apiconnect.rb
@@ -8,7 +8,7 @@ class GetRequester
 
 
   def initialize(api_key)
-    @url = "https://www.superheroapi.com/api.php/#{api_key}/search/howard"
+    @url = "https://www.superheroapi.com/api.php/#{api_key}/search/howard" # they want commented code.  Switched up the argument since this is no longer a multiple purpose method.  This is going to be 100% for searching superheroes.  Rather than taking an json link as an argument, I'm instead forcing it to my desired API, with the argument now being the API key.
 
 
   end
