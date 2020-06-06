3b537e3429d7102252264b88fe00cdc6341cb057
kyle <kyledmclean@gmail.com>
2020-06-06 03:36:25 -0500

    Commit to something stable before working on search

--- lib/cli/apiconnect.rb
+++ lib/cli/apiconnect.rb
@@ -6,6 +6,16 @@ class Api
   KEY = ENV['SUPER_KEY']
   BASE_URL = 'https://www.superheroapi.com/api.php/'
   
+  def self.base  #Just give us some base results.
+    res = RestClient.get("#{BASE_URL}#{KEY}/search/death")
+    data = JSON.parse(res.body)
+    data['results'].each do |champ|
+      name = champ['name']
+      id = champ['id']
+      Champion.new(id, name)
+    end
+
+  end
 
 
   def self.search(name)  #search results.
--- lib/cli/cli.rb
+++ lib/cli/cli.rb
@@ -1,20 +1,17 @@
 class Cli
 
     def run
-        intro
-        Api.search("death") # have to come up with a search a bit later, hardcoding for the time being
+        print_intro
+        Api.base # Switched to some base results
         main
-    end
-
-    def intro
-        puts "Available Champions"
     end
 
     def main
         print_all
         print_selection_prompt
         name = selection_prompt
-       binding.pry
+    #   binding.pry
+        valid_name?(name)
     end
 
     def valid_name?(name)  #trying to switch validation to name
@@ -22,11 +19,20 @@ class Cli
        if Champion.all.any? {|c| name == c.name }
             true
         else
-            false
+            print_bad_name
+            sleep 2.0
+            main
         end
 
     end
     
+    def print_intro
+        puts "Available Champions"
+    end
+
+    def print_search
+        puts "Search Available Champions"
+    end
 
     def print_all
         Champion.all.each {|c| puts "#{c.id}) #{c.name}" }
@@ -34,6 +40,10 @@ class Cli
 
     def print_selection_prompt
         puts "Select a Champion by Name"
+    end
+
+    def print_bad_name
+        puts "Poor selection, try again"
     end
 
     def selection_prompt
