dda57620fa83347cf8c06a69af442ea8466218b0
kyle <kyledmclean@gmail.com>
2020-06-06 02:42:37 -0500

    Base CLI

--- lib/cli/cli.rb
+++ lib/cli/cli.rb
@@ -1,4 +1,21 @@
 class Cli
 
+    def run
+        intro
+        Api.search("death") # have to come up with a search a bit later, hardcoding for the time being
+        main
+    end
+
+    def intro
+        puts "Select Your Champions"
+    end
+
+    def main
+        print_all
+    end
+
+    def print_all
+        Champion.all.each {|c| puts "#{c.id}) #{c.name}" }
+    end
 
 end
