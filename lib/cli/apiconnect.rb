f238ac57a5e50159bc8c2e503c26a88e421e1cf4
kyle <kyledmclean@gmail.com>
2020-06-06 02:33:57 -0500

    Simple Champion creation.  ID/Name

--- lib/cli/apiconnect.rb
+++ lib/cli/apiconnect.rb
@@ -1,44 +1,25 @@
 #require 'dotenv/load' # BYYYYYE
  
 class Api
-  attr_accessor :url
+  # attr_accessor :url # remnant of some of the original tests
 
   KEY = ENV['SUPER_KEY']
   BASE_URL = 'https://www.superheroapi.com/api.php/'
   
 
- # def initialize(search_term)
-#
- #   @url = "#{BASE_URL}#{KEY}/search/#{search_term}" # they want commented code.  Switched up the argument since this is no longer a multiple purpose method.  This is going to be 100% for searching superheroes.  Rather than taking an json link as an argument, I'm instead forcing it to my desired API, with the argument now being the API key.
-  #  puts @url
-#
- # end
 
   def self.search(name)  #search results.
     res = RestClient.get("#{BASE_URL}#{KEY}/search/#{name}")
     data = JSON.parse(res.body)
     data['results'].each do |champ|
-      puts champ['name']
-      puts champ['id']
+      name = champ['name']
+      id = champ['id']
+      Champion.new(id, name)
     end
-   # binding.pry
+
   end
 
-
-#  def get_response_body
-#    uri = URI.parse(@url)
-#    response = Net::HTTP.get_response(uri)
-#    response.body
-#  end
-
-#  def parse_json
-#    parsed_json = JSON.parse(self.get_response_body)
-#    parsed_json
-#  end
 
 
 
 end
-
-#howard = Api.new('howard')
-#print howard.parse_json
--- lib/cli/champion.rb
+++ lib/cli/champion.rb
@@ -1,4 +1,18 @@
 class Champion
+    attr_accessor :id, :name
+
+    @@all = []
+    def initialize(id, name)
+        @id = id
+        @name = name
+        @@all << self
+
+    end
+
+    def self.all
+        @@all
+    end
+    
 
 
 end
