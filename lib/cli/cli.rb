b4aa71b05298a0ec744331bf2b13de8b1d16088d
kyle <kyledmclean@gmail.com>
2020-06-06 03:19:49 -0500

    Functioning name validation

--- lib/cli/cli.rb
+++ lib/cli/cli.rb
@@ -7,15 +7,38 @@ class Cli
     end
 
     def intro
-        puts "Select Your Champions"
+        puts "Available Champions"
     end
 
     def main
         print_all
+        print_selection_prompt
+        name = selection_prompt
+       binding.pry
     end
+
+    def valid_name?(name)  #trying to switch validation to name
+       # id = id.to_i
+       if Champion.all.any? {|c| name == c.name }
+            true
+        else
+            false
+        end
+
+    end
+    
 
     def print_all
         Champion.all.each {|c| puts "#{c.id}) #{c.name}" }
     end
+
+    def print_selection_prompt
+        puts "Select a Champion by Name"
+    end
+
+    def selection_prompt
+        gets.chomp
+    end
+
 
 end
