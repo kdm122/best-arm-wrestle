1c75775e671f3b10c59dab1b65abed7f0a7a3a4a
kyle <kyledmclean@gmail.com>
2020-06-07 22:35:35 -0500

    Working on getting search by name/add to tournament participants working

--- lib/cli/apiconnect.rb
+++ lib/cli/apiconnect.rb
@@ -11,6 +11,7 @@ class Api
 #    binding.pry # inserting this here because I'm about to start checking into RestClient response codes.  Need to be able to handle failed seearches.    
     data = JSON.parse(res.body)
 #    data["response"] # checking if this was error/success
+#binding.pry
     data['results'].each do |champ|
       name = champ['name']
       id = champ['id']
@@ -24,15 +25,28 @@ class Api
 
 
   def self.search_by_name(name)  #search results.
-    res = RestClient.get("#{BASE_URL}#{KEY}/search/#{name}")
+    name_space = name.split(' ').join('%20')
+    res = RestClient.get("#{BASE_URL}#{KEY}/search/#{name_space}")
     data = JSON.parse(res.body)
-    data['results'].each do |champ|
+    if data['results'].count > 1
+      puts "Too many results.  Select with more specificity from the following:"
+      data['results'].each do |champ|
+        puts "#{champ['name']}"
+      end
+      
+    else
+      data['results'].each do |champ| # leftover junk
       name = champ['name']
       id = champ['id']
-      Champion.new(id, name)
+      powerstats = champ['powerstats']
+      appearance = champ['appearance']
+      Champion.new(id, name, powerstats, appearance)
+      end
     end
 
   end
+
+  #
 
   
 
